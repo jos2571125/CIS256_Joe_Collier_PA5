a16f676ed937433eade81e073c726a184e5006f1
Joe Collier <jos2571125@maricopa.edu>
2025-11-16 17:48:22 -0700

    Updated notes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
+# Form template
 form_html = '''
 <form method="POST" action="/login">
     <label for="username">Username:</label>
@@ -23,7 +24,7 @@ form_html = '''
         <p><strong>Hashed Password:</strong> {{ result.password_hash }}</p>
     {% endif %}
 '''
-# Password 
+
 # Route to display the login form
 @app.route('/login', methods=['GET'])
 def login_form():
@@ -35,7 +36,7 @@ def login():
     username = request.form['username']
     password = request.form['password']
 
-    # Check username for only letters, numbers, or underscores
+    # Check username for only letters, numbers, and underscores
     allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
     for char in username:
         if char not in allowed_chars:
@@ -47,29 +48,32 @@ def login():
         error = "Password must be at least 8 characters long."
         return render_template_string(form_html, error=error)
     
-    # check password has both letters and numbers
+    # Variables for letter and number checks
     has_letter = False
     has_number = False
 
+    # Loop to check each character for letters and numbers
     for char in password:
         if char.isalpha():
             has_letter = True
         if char.isdigit():
             has_number = True
     
+    # Check and error handling when no letters or number is found
     if not has_letter or not has_number:
         error = "Password must contain both letters and numbers."
         return render_template_string(form_html, error=error)
 
+    # Hash inputed password
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
+    # Provide result of username, password, and password hash
     result = {
         "username": username,
         "password": password,
         "password_hash": hashed_password
     }
 
-    # Handle the login logic here
     return render_template_string(form_html, result=result)
 
 if __name__ == '__main__':
